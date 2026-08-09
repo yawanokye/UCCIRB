24f575f8788d00f0597466dec9c04ae308bbbbc8
yawanokye <aadam@ucc.edu.gh>
2026-08-09 22:13:33 +0000

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 from starlette.middleware.sessions import SessionMiddleware
 
 from .config import APP_NAME, BASE_DIR, SECRET_KEY, SESSION_HTTPS_ONLY, STORAGE_DIR
@@ -16,3 +17,8 @@ app.mount('/static', StaticFiles(directory=BASE_DIR / 'app' / 'static'), name='s
 app.state.templates = Jinja2Templates(directory=BASE_DIR / 'app' / 'templates')
 app.include_router(auth.router)
 app.include_router(portal.router)
+
+
+@app.get('/healthz', include_in_schema=False)
+def healthz():
+    return JSONResponse({'status': 'ok'})
